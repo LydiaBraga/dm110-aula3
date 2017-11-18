49accb32b459c5b7f2e4f763026d5defdc42feb6
Aluno do Inatel <aluno@local.inatel.br>
2017-11-18 13:19:43 -0200

    Added jms.

--- dm110-ejb/src/main/java/br/inatel/dm110/hello/beans/HelloBean.java
+++ dm110-ejb/src/main/java/br/inatel/dm110/hello/beans/HelloBean.java
@@ -1,5 +1,6 @@
 package br.inatel.dm110.hello.beans;
 
+import javax.ejb.EJB;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
@@ -11,10 +12,13 @@ import br.inatel.dm110.hello.interfaces.HelloRemote;
 @Remote(HelloRemote.class)
 @Local(HelloLocal.class)
 public class HelloBean implements HelloLocal, HelloRemote {
+	
+	@EJB
+	private HelloMessageSender messageSender;
 
 	@Override
 	public String sayHello(String name) {
-		System.out.println("Inside EJB!!!");
+		messageSender.sendTextMessage(name);
 		return "Hello " + name + "!!!";
 	}
 
